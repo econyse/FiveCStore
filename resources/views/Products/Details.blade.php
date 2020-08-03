eec8994957f2f333b15aa061c7f07d3b59c3557d
econyse <pacofep16@gmail.com>
2020-08-03 12:55:31 -0500

    rating

--- resources/views/Products/Details.blade.php
+++ resources/views/Products/Details.blade.php
@@ -9,13 +9,32 @@
                     <h6 class="card-subtitle mb-2 text-muted">${{ $product->price." ".$product->currency}}</h6>
                     <p class="card-text">{{ $product->description }}</p>
                 </div>
-                <div class="card-footer">
+                <div id="starRating" class="card-footer">
                     <p>Rating: </p>
-                    <input type="radio" name="rating" id="rating">
-                    <input type="radio" name="rating" id="rating">
-                    <input type="radio" name="rating" id="rating">
-                    <input type="radio" name="rating" id="rating">
-                    <input type="radio" name="rating" id="rating">
+                    <span class="fa-2x text-warning">
+                        <i class="far fa-star" onclick="setStarRating(2)" onmouseleave="starRating(document.getElementById('ratingval').value);" onmouseover="starRating(2);"></i>
+                    </span>
+                    <span class="fa-2x text-warning">
+                        <i class="far fa-star" onclick="setStarRating(4)" onmouseleave="starRating(document.getElementById('ratingval').value);" onmouseover="starRating(4);"></i>
+                    </span>
+                    <span class="fa-2x text-warning">
+                        <i class="far fa-star" onclick="setStarRating(6)" onmouseleave="starRating(document.getElementById('ratingval').value);" onmouseover="starRating(6);"></i>
+                    </span>
+                    <span class="fa-2x text-warning">
+                        <i class="far fa-star" onclick="setStarRating(8)" onmouseleave="starRating(document.getElementById('ratingval').value);" onmouseover="starRating(8);"></i>
+                    </span>
+                    <span class="fa-2x text-warning">
+                        <i class="far fa-star" onclick="setStarRating(10)" onmouseleave="starRating(document.getElementById('ratingval').value);" onmouseover="starRating(10);"></i>
+                    </span>
+                    @php
+                        $rating = (array)$product->rating;
+                        if (count($rating) == 0)
+                            $avg = 0;
+                        else
+                            $avg = array_sum($rating) / count($rating);
+                    @endphp
+
+                    <input type="number" name="ratingval" id="ratingval" min="0" max="10" hidden value="{{ $avg }}">
                 </div>
             </div>
             <div class="col-md-12">
@@ -52,4 +71,38 @@
             </div>
         </div>
     </div>
+    <script>
+        function starRating(rating) {
+            var elStars = document.getElementById("starRating").getElementsByTagName("i");
+            for (var i = 0; i < elStars.length; i++) {
+                elStars[i].setAttribute("class", "far fa-star");
+            }
+            for (i = 1; i <= Math.ceil(rating/2); i++){
+                let elStar = elStars[i - 1];
+                if (i == Math.ceil(rating/2) && rating % 2 == 1){
+                    elStar.setAttribute("class", "fas fa-star-half-alt")
+                } else {
+                    elStar.setAttribute("class", "fas fa-star")
+                }
+            }
+        }
+        function setStarRating(rating){
+            const prodID = "{{ $product->_id }}"
+            var formData = {
+                id: prodID,
+                rating: rating
+            }
+            axios.post("/api/rating",formData)
+                .then(function (response) {
+                    document.getElementById("ratingval").value = response.data.avg;
+                    starRating(response.data.avg);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+        }
+        (function () {
+            starRating(document.getElementById("ratingval").value);
+        })();
+    </script>
 @endsection
